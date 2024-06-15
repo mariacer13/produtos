9a7753b170b943816e6718f403293e5f35596272
mariacer13 <143567571+mariacer13@users.noreply.github.com>
2024-06-15 14:52:47 -0500

    Update config.php

--- data/config.php
+++ data/config.php
@@ -6,12 +6,12 @@ class Connection
         #$options = array(
        # PDO::MYSQL_ATTR_SSL_CA => '../ssl/BaltimoreCyberTrustRoot.crt.pem'
 #);
-        $DB_DSN = getenv('db_dsn');
-        $DB_USERNAME = getenv('db_username');
-        $DB_PASSWORD = getenv('db_password');
-        $dsn =$DB_DSN;
-        $username = $DB_USERNAME;
-        $password =$DB_PASSWORD;
+        #$DB_DSN = getenv('db_dsn');
+        #$DB_USERNAME = getenv('db_username');
+        #$DB_PASSWORD = getenv('db_password');
+        #$dsn =$DB_DSN;
+        #$username = $DB_USERNAME;
+        #$password =$DB_PASSWORD;
         
         try {
             $connection = new PDO("mysql:host=dbproductosml.mysql.database.azure.com;dbname=db_personaproducto",
