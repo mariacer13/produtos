ae4b523178c6736d5628cc652d6645c9c85ad9cf
mariacer13 <143567571+mariacer13@users.noreply.github.com>
2024-06-15 12:36:01 -0500

    Update config.php

--- data/config.php
+++ data/config.php
@@ -3,9 +3,9 @@ class Connection
 {
     static function _connect()
     {
-        $options = array(
-        PDO::MYSQL_ATTR_SSL_CA => '../ssl/BaltimoreCyberTrustRoot.crt.pem'
-);
+        #$options = array(
+       # PDO::MYSQL_ATTR_SSL_CA => '../ssl/BaltimoreCyberTrustRoot.crt.pem'
+#);
         $DB_DSN = getenv('db_dsn');
         $DB_USERNAME = getenv('db_username');
         $DB_PASSWORD = getenv('db_password');
@@ -14,7 +14,7 @@ class Connection
         $password =$DB_PASSWORD;
         
         try {
-            $connection = new PDO('mysql:host=dbproductosml.mysql.database.azure.com;port=3306;dbname=db_personaproducto', 'administrador', '12345678ml_',$options);
+            $connection = new PDO('mysql:host=dbproductosml.mysql.database.azure.com;port=3306;dbname=db_personaproducto', 'administrador', '12345678ml_');
             //echo "conectado";
             return $connection;
         } catch (\Throwable $th) {
