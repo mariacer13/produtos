cf98990fe3f41e2c8f7d681dbb3fe04bd24a8868
mariacer13 <143567571+mariacer13@users.noreply.github.com>
2024-06-17 02:09:30 -0500

    Update sha.php

--- data/sha.php
+++ data/sha.php
@@ -12,13 +12,13 @@ class SED
         $iv = substr(hash('sha256', SECRET_IV), 0, 16);
         $output = openssl_encrypt($string, METHOD, $key, 0, $iv);
         $output = base64_encode($output);
-        return $output;
+        return $string;
     }
     public static function decryption($string)
     {
         $key = hash('sha256', SECRET_KEY);
         $iv = substr(hash('sha256', SECRET_IV), 0, 16);
         $output = openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
-        return $output;
+        return $string;
     }
 }
