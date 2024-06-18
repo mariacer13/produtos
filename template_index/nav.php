ce35b356968682bad863421fb2f8bbd1a472b48b
mariacer13 <143567571+mariacer13@users.noreply.github.com>
2024-06-17 22:05:19 -0500

    Update nav.php

--- template_index/nav.php
+++ template_index/nav.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['user'])) {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="main.php">Start</a>
+                    <a class="nav-link" href="main.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="products.php">Products</a>
