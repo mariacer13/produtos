30d7f2a92a04e3db921703addc72359fc5c99e83
mariacer13 <143567571+mariacer13@users.noreply.github.com>
2024-06-17 22:02:59 -0500

    Update config.php

--- data/config.php
+++ data/config.php
@@ -6,10 +6,14 @@ class Connection
         #$options = array(
        # PDO::MYSQL_ATTR_SSL_CA => '../ssl/BaltimoreCyberTrustRoot.crt.pem'
 #);
+
+        // Variables de entorno para la conexión a la base de datos
         $DB_NAME=getenv('db_name');
         $DB_HOST=getenv('db_host');
         $DB_USERNAME = getenv('db_username');
         $DB_PASSWORD = getenv('db_password');
+
+       // Construcción del DSN (Data Source Name) y asignación de credenciales
         $dsn ="mysql:host=$DB_HOST;dbname=$DB_NAME";
         $username = $DB_USERNAME;
         $password =$DB_PASSWORD;
